bf1a467be4e1ea89e961fd93ce16b44f5da5c0bc
Covarrubias Rodriguez <jcovarrubiasrodri@deloitte.com>
2023-05-31 15:33:10 -0600

    otro ejemplo

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-type bot interface {
+/*type bot interface {
 	getGreeting() string
 }
 
@@ -29,4 +29,38 @@ func main() {
 	printGreeting(eb)
 	printGreeting(sb)
 
+}*/
+
+type FiguraGeometrica interface {
+	area() float64
+}
+
+type Rectangulo struct {
+	base, altura float64
+}
+
+type Trapecio struct {
+	baseMayor, baseMenor, altura float64
+}
+
+func (figura Rectangulo) area() float64 {
+	return figura.base * figura.altura
+}
+
+func (figura Trapecio) area() float64 {
+	return (figura.baseMayor + figura.baseMenor) * figura.altura / 2
+}
+
+func dameArea(fig FiguraGeometrica) float64 {
+	return fig.area()
+}
+
+func main() {
+	rectangulo := Rectangulo{base: 4, altura: 7.5}
+	trapecio := Trapecio{baseMayor: 5, baseMenor: 2, altura: 3}
+
+	fmt.Printf("area del rectangulo %f\n", dameArea(rectangulo))
+
+	fmt.Printf("area del trapecio %f\n", trapecio.area())
+	fmt.Printf("area del trapecio %f\n", dameArea(trapecio))
 }
